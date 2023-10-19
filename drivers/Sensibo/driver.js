0ab3088f3c7f1b452526ad99bf2cbbab3958f08a
Tapio Heiskanen <tapio.heiskanen@iki.fi>
2023-10-19 07:38:58 +0300

    Updated sample-data.json with more productModels

--- drivers/Sensibo/driver.js
+++ drivers/Sensibo/driver.js
@@ -7,7 +7,7 @@ module.exports = class SensiboDriver extends BaseDriver {
   driverName = () => 'SensiboDriver';
 
   filterProductModel = (device) => {
-    // sky = Sensibo Sky
+    // sky, skyv2 = Sensibo Sky
     // skyplus = Sensibo Air
     // airq = Sensibo AirQ and Sensibo Air Pro
     return device.productModel && (device.productModel.includes('sky') || device.productModel.includes('airq'));
